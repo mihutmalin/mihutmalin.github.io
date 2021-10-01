5b991b9c5e1ae0095708cdb53d4a5a84ac8326bf
mihutmalin <mihutmalin@yahoo.com>
2021-10-01 13:41:55 +0300

    Implementation site from youtube/template

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import image from "../background.jpg";
+
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="backgroundIMG"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-height-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Aloha. I'm Mălin
+        </h1>
+      </section>
+    </main>
+  );
+}
